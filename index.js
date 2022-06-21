d5107b09d2e38c6f96d3b431d80611df2d5016f7
sagar2955 <sagar.gunjal@morningstar.com>
2022-06-21 13:13:57 +0530

    login

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+require("dotenv").config();
+
+const app = express()
+const userRoute = require('./app/api/routes/users')
+
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+app.set('secretKey','hdjsakfhdjsk')
+const userValidation = (req, res,next) => {
+    jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), 
+    (err,decoded) =>{
+        if(err){
+            res.json({
+                message: err
+            })
+        }
+        next()
+    })
+}
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use('/user',userRoute)
+
+
+
+app.get('/home', (req,res) => {
+    res.json({
+        "APP": "JWT Based API Application",
+        "message": "Successfully Running the Application"
+    })
+})
+
+const mongoURI = "mongodb+srv://sagargunjal2955:sagar@cluster0.7a5skea.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURI)
+.then(() => {
+    console.log("Successfully Connected to the Databas")
+})
+.catch((err) => {
+    console.log(err)
+})
+
+app.listen(5000,() => {
+    console.log("Successfully Running on the PORT: 5000")
+})
